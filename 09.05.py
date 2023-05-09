bdffc355601973b43ab6f8718918e9468566f030
Hrechanyuk Anastasiia <anastasiya.grk@gmail.com>
2023-05-09 18:52:06 +0300

    додали клас Student та вивели його по імені в методі admit__student

--- 09.05.py
+++ 09.05.py
@@ -5,9 +5,13 @@ class School:
         self.students + students #список студентів
     def admit__student(self, student):
         self.student.append(student)
-        print(f'{}') #дописати, коли створимо клас студентів
+        print(f'{student.name} був допущений до школи {self.name}') #дописати, коли створимо клас студентів
 
 class Student:
+    def __init__(self, name, grade):
+        self.name = name
+        self.grade = grade
+
 
 
 
