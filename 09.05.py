dad3f8bfe6a5ff55cec4e2b93d91e140702b501e
Hrechanyuk Anastasiia <anastasiya.grk@gmail.com>
2023-05-09 18:46:18 +0300

    створений клас school, можливість додавання до них студенів

---
+++ 09.05.py
@@ -0,0 +1,13 @@
+#симулятор навчального закладу (база даних)
+class School:
+    def __init__(self, name, students):
+        self.name = name
+        self.students + students #список студентів
+    def admit__student(self, student):
+        self.student.append(student)
+        print(f'{}') #дописати, коли створимо клас студентів
+
+class Student:
+
+
+
