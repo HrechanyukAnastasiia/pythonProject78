51e990866e8e0369471a0a0e8973745e6db2cfcb
Hrechanyuk Anastasiia <anastasiya.grk@gmail.com>
2023-05-09 19:20:34 +0300

    знайомство з filter()

--- 09.05.py
+++ 09.05.py
@@ -28,5 +28,8 @@ class Student:
 
 multiplay = lambda x, y: x * y
 print(multiplay(2,5))
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+filtered_numbers = list(filter(lambda x : x % 2 == 0, numbers))
+print(filtered_numbers)
 
 
